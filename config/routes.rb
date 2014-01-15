ab8e4bb7b99db24cbc415bc597c20872e25a4095
chrispnicholson <chris.nicholson@gmail.com>
2014-01-15 18:40:03 +0000

    Adding home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Collectiveopus::Application.routes.draw do
+  get "home/index"
+
   resources :gifts
 
 
@@ -61,6 +63,7 @@ Collectiveopus::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root :to => 'home#index', as: 'home'
 
   # See how all your routes lay out with "rake routes"
 
